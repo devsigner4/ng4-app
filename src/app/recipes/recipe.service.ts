60845ddf4422e7b00a3cccb574f31bf0fae22e5b
jsh <jshdream4@naver.com>
2017-07-24 20:18:51 +0900

    section10 110chapter

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,41 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter , Injectable} from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Grunt', 'This is grunt logo', 'https://amsterdam.luminis.eu/wp-content/uploads/2014/12/grunt_logo.jpg'),
-    new Recipe('bower', 'This is bower logo', 'https://bower.io/img/bower-logo.png')
+    new Recipe(
+      'Grunt',
+      'This is grunt logo',
+      'https://amsterdam.luminis.eu/wp-content/uploads/2014/12/grunt_logo.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'bower',
+      'This is bower logo',
+      'https://bower.io/img/bower-logo.png',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meats', 1)
+      ])
   ];
+
+  constructor(private slService: ShoppingListService){};
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
